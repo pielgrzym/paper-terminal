02c48663d56a2e753a17dbabcfb154ea857ea8bd
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2017-09-30 00:55:46 +0200

    hopefully a more legible login class logic

--- paperterm/login_screen.py
+++ paperterm/login_screen.py
@@ -20,40 +20,50 @@ class LoginScreen(object):
             termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
         return ch
 
+    def authenticate(self):
+        if pam.authenticate(self.username, self.password):
+            logging.debug("Auth success for user: %s" % self.username)
+            self.password = None # such security! :D
+            self.authenticated = True
+        else:
+            logging.debug("Auth FAIL for user: %s" % self.username)
+            self.authenticated = False
+            self.display_q.put("Wrong user/pass\n\r")
+
+    def get_username(self):
+         self.username = ""
+         ignore_counter = 0
+         while True:
+             c = self.getchr()
+             logging.debug("[Login screen] Typed char: %s (ord: %d)" % (c, ord(c)))
+             if ignore_counter > 0:
+                 ignore_counter -= 1
+                 continue
+             if c == "\r":
+                 self.display_q.put("\n\r")
+                 break
+             elif ord(c) == 127: # backspace, delete one char
+                 self.username = self.username[:-1]
+                 # yes, to erase a char in terminal, one needs to do a
+                 # backspace a space and a backspace...
+                 c = "\b \b"
+             elif ord(c) == 27: # arrows produce 3 chars, so we need to skip 3 inputs total
+                 ignore_counter = 2
+                 continue
+             else:
+                 self.username += c
+             self.display_q.put(c)
+
+    def get_password(self):
+        self.password = getpass("Password: ")
+
     def run(self):
         while True:
             self.display_q.put("Login: ")
             print("Login: ")
-            self.username = ""
-            ignore_counter = 0
-            while True:
-                c = self.getchr()
-                logging.debug("[Login screen] Typed char: %s (ord: %d)" % (c, ord(c)))
-                if ignore_counter > 0:
-                    ignore_counter -= 1
-                    continue
-                if c == "\r":
-                    self.display_q.put("\n\r")
-                    break
-                elif ord(c) == 127: # backspace, delete one char
-                    self.username = self.username[:-1]
-                    # yes, to erase a char in terminal, one needs to do a
-                    # backspace a space and a backspace...
-                    c = "\b \b"
-                elif ord(c) == 27: # arrows produce 3 chars, so we need to skip 3 inputs total
-                    ignore_counter = 2
-                    continue
-                else:
-                    self.username += c
-                self.display_q.put(c)
+            self.get_username()
             self.display_q.put("Password:\n\r")
-            self.password = getpass("Password: ")
-            if pam.authenticate(self.username, self.password):
-                logging.debug("Auth success for user: %s" % self.username)
-                self.password = None # such security! :D
-                self.authenticated = True
+            self.get_password()
+            self.authenticate()
+            if self.authenticated:
                 break
-            else:
-                logging.debug("Auth FAIL for user: %s" % self.username)
-                self.authenticated = False
-                self.display_q.put("Wrong user/pass\n\r")
