7f82ae04f7d3920f94cc6fec3df035e4f645f718
Jakub Nawalaniec <jakub.nawalaniec@getbase.com>
2017-09-29 21:47:53 +0200

    debug

--- paperterm/display_thread.py
+++ paperterm/display_thread.py
@@ -105,6 +105,8 @@ class DisplayThread(threading.Thread):
         self.image = Image.new('1', (epd2in9.EPD_HEIGHT, epd2in9.EPD_WIDTH), 255)
         self.draw = ImageDraw.Draw(self.image)
         self.draw.multiline_text((0, 0), "\n".join(input_list), font=self.font)
+        logging.debug("Cursor: x: %d, y: %d [x px: %d, y px: %d]",
+                self.screen.cursor.x, self.screen.cursor.y, self.screen.cursor.x*12, self.screen.cursor.y*12)
         self.draw.rectangle((self.screen.cursor.x*12, self.screen.cursor.y*12, (self.screen.cursor.x+1)*12, (self.screen.cursor.y+1)*12),
                 fill=0)
         logging.debug("To be redrawed ...")
